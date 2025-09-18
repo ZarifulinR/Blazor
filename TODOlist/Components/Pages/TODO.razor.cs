119bb90e737267b09e43744e66135bba8ca0eaa7
ZarifulinR <fred-c112@yandex.ru>
2025-09-18 16:22:48 +0300

    WomeW

---
+++ TODOlist/Components/Pages/TODO.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace TODOlist.Components.Pages
+{
+    public partial class TODO
+    {
+        private  List<TODOItem> tasks = [];
+        private string task;
+        private void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(task))
+            {
+                tasks.Add(new TODOItem { Title = task });
+                task = string.Empty;
+            }
+        }
+    }
+}
