692d35aa3003c2b154add1c6a0b335cb4c58856a
ZarifulinR <fred-c112@yandex.ru>
2025-09-19 15:26:29 +0300

    Connect to DB DONE

---
+++ Academy/Migrations/20250919121356_initialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Academy.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Discipline",
+                columns: table => new
+                {
+                    discipline_id = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    discipline_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    number_of_lessons = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discipline", x => x.discipline_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Discipline");
+        }
+    }
+}
---
+++ Academy/Models/Discipline.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Academy.Models
+{
+    public class Discipline
+    {
+        [Key]   
+        public short discipline_id {  get; set; }
+        public string? discipline_name { get; set; }
+        public int number_of_lessons { get; set; }  
+
+    }
+}
