a9f8a2e23afddeba0b9cb46f8c7ef43d48219680
ZarifulinR <fred-c112@yandex.ru>
2025-09-19 12:30:19 +0300

    Add Movie DONE

---
+++ Mavies/Data/MaviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mavies;
+
+namespace Mavies.Data
+{
+    public class MaviesContext : DbContext
+    {
+        public MaviesContext (DbContextOptions<MaviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mavies.Movie> Movie { get; set; } = default!;
+    }
+}
