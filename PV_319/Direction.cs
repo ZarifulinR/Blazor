e148b41a4ff5cf1c7a39562cfacd817e946c3861
ZarifulinR <fred-c112@yandex.ru>
2025-09-25 22:04:53 +0300

    work in class

---
+++ PV_319/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PV_319;
+
+public partial class Direction
+{
+    [Key]
+    [Required]
+    public byte direction_id { get; set; }
+    [Required]
+    public string? direction_name { get; set; }
+}
